8225a5a18f0298719934a05921f0b0262f1be80b
AraceliGS <aragutarras@gmail.com>
2017-12-02 17:34:45 -0500

    añadiendo cambios al main.css

--- js/app.js
+++ js/app.js
@@ -17,24 +17,85 @@ window.onload = function() {
     var square10 = document.getElementById('square-10');
     var square11 = document.getElementById('square-11');
     var square12 = document.getElementById('square-12');
-    if (square1) {
-      if (ascii === 38) {
-        square.classList.add('red');
-        square.classList.remove('white');
-      } else if (ascii === 40) {
-        square.classList.add('red');
-        square.classList.remove('white');
-      } else if (ascii === 37) {
-        square.classList.add('red');
-        square.classList.remove('white');
-      } else if (ascii === 39) {
-        square.classList.add('red');
-        square.classList.remove('white');
-      } else {
-        square.classList.add('white');
-        square.classList.remove('red');
+    var squares = document.querySelectorAll('div');
+    for (var i = 0; i < squares.length;i++) {
+      if (squares[0] === square1) {
+        if (ascii === 40) {
+          square5.classList.add('red');
+          square5.classList.remove('white');
+        } else if (ascii === 39) {
+          square2.classList.add('red');
+          square2.classList.remove('white');
+          square3.classList.add('white');
+          square4.classList.add('white');
+        } else {
+          square3.classList.add('white');
+          square4.classList.add('white');
+          // square6.classList.add('white');
+          // square7.classList.add('white');
+          // square8.classList.add('white');
+          // square10.classList.add('white');
+          // square11.classList.add('white');
+          // square12.classList.add('white');
+          square3.classList.remove('red');
+          square4.classList.remove('red');
+          // square6.classList.remove('red');
+          // square7.classList.remove('red');
+          // square8.classList.remove('red');
+          // square10.classList.remove('red');
+          // square11.classList.remove('red');
+          // square12.classList.remove('red');
+        }
       }
     }
+
+    if (squares[1] === square2) {
+      if (ascii === 40) {
+        square6.classList.add('red');
+        square6.classList.remove('white');
+      } else if (ascii === 37) {
+        square1.classList.add('red');
+        square1.classList.remove('white');
+      } else if (ascii === 39) {
+        square3.classList.add('red');
+        square3.classList.remove('white');
+        square2.classList.add('white');
+        square4.classList.add('white');
+      } else {
+
+      }
+    }
+    // if (square1) {
+    //   if (ascii === 38) { arriba
+    //     square.classList.add('red');
+    //     square.classList.remove('white');
+    //   } else if (ascii === 40) { abajo
+    //     square.classList.add('red');
+    //     square.classList.remove('white');
+    //   } else if (ascii === 37) { izquierda
+    //     square.classList.add('red');
+    //     square.classList.remove('white');
+    //   } else if (ascii === 39) { derecha
+    //     square.classList.add('red');
+    //     square.classList.remove('white');
+    //   } else {
+    //     square.classList.add('white');
+    //     square.classList.remove('red');
+    //   }
+
+    /*
+    if (ascii === 38) {
+      square5.classList.add('red');
+      square5.classList.remove('white');
+    }
+    */
+
+    /*
+    else {
+     square.classList.add('white');
+     square.classList.remove('red');
+    }
+    */
   });
   // var tdElements = document.getElementsByTagName('td');
   // var onlyOneSquare;
@@ -46,3 +107,20 @@ window.onload = function() {
   //   });
   // }
 };
+/* square3.classList.add('white');
+   square4.classList.add('white');
+   square6.classList.add('white');
+   square7.classList.add('white');
+   square8.classList.add('white');
+   square10.classList.add('white');
+   square11.classList.add('white');
+   square12.classList.add('white');
+   square3.classList.remove('red');
+   square4.classList.remove('red');
+   square6.classList.remove('red');
+   square7.classList.remove('red');
+   square8.classList.remove('red');
+   square10.classList.remove('red');
+   square11.classList.remove('red');
+   square12.classList.remove('red');
+*/
